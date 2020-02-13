71a4721100dff818db3489d2e04441b43b92f611
Thomas CARPENTIER <tcarpentier@genymobile.com>
2020-02-13 15:52:45 +0100

    Fix interpolation warnings in examples

--- examples/StartOneGenymotionCloud/main.tf
+++ examples/StartOneGenymotionCloud/main.tf
@@ -2,13 +2,14 @@ provider "genymotion" {
 }
 
 variable "recipe_uuid" {
-  type    = "string"
+  type    = string
   default = "107d757e-463a-4a18-8667-b8dec6e4c87e"
 }
 
 resource "genymotion_cloud" "Android70" {
-  recipe_uuid = "${var.recipe_uuid}"
-  name     = "DeviceConnectedWithAdb"
-  adbconnect = true
+  recipe_uuid     = var.recipe_uuid
+  name            = "DeviceConnectedWithAdb"
+  adbconnect      = true
   adb_serial_port = "7090"
 }
+
--- examples/StartSeveralGenymotionCloudDevices/main.tf
+++ examples/StartSeveralGenymotionCloudDevices/main.tf
@@ -1,23 +1,24 @@
-provider "genymotion" { 
+provider "genymotion" {
   email       = "myemail@mycompany"
   password    = "mypassword"
 }
 
 resource "genymotion_cloud" "Android70" {
   recipe_uuid = "9f1adea8-e280-460d-9319-580570f61e8c"
-  name     = "Android70-ADB-Connected"
-  adbconnect = true
+  name        = "Android70-ADB-Connected"
+  adbconnect  = true
 }
 
 resource "genymotion_cloud" "Android80" {
-  recipe_uuid = "e7a4ecd9-6044-41c7-ace3-ccee5402b590"
-  name     = "Android70-ADB-Connected-with-serial-port"
-  adbconnect = true
+  recipe_uuid     = "e7a4ecd9-6044-41c7-ace3-ccee5402b590"
+  name            = "Android70-ADB-Connected-with-serial-port"
+  adbconnect      = true
   adb_serial_port = "9090"
 }
 
 resource "genymotion_cloud" "Android90" {
   recipe_uuid = "bd402826-4ee6-4598-94df-da4f89021042"
-  name     = "Android90-ADB-Not-Connected"
-  adbconnect = false
+  name        = "Android90-ADB-Not-Connected"
+  adbconnect  = false
 }
+
